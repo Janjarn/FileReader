fe7d17ffc5ce21d4432db8c9e739647eb5da3f79
Janjarn <morgre01@easv365.dk>
2022-11-15 21:42:53 +0100

    jh,gv

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,54 @@ import java.nio.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 import static java.nio.file.StandardOpenOption.APPEND;
 
 public class Main {
-    private static String fileStringPath = "C:\\Users\\Morte\\Desktop\\MyFile.txt";
+    private static String fileStringPath = "C:\\Users\\Morte\\Desktop\\MyFile.ser";
     private static Path filePath = Path.of(fileStringPath);
     public static void main(String[] args) {
-        printFileContents();
-        //addToFile("Some nio line");
+        /*Person p = new Person(1, "Jeppe");
+        Person p2 = new Person(2, "Peter");
+        Person p3 = new Person(3, "Jane");
+
+        List<Person> persons = new ArrayList<>();
+        persons.add(p);
+        persons.add(p2);
+        persons.add(p3);
+
+        System.out.println(persons);
+
+        //savePersons(persons);
+*/
+        List<Person> personsFromFile = loadPersons();
+        System.out.println(personsFromFile);
+        /*printFileContents();
+        addToFile("Some nio line");
         addTextAtLine("Weird new nio line write", 4);
-        printFileContents();
+        printFileContents();*/
+    }
+
+    private static List<Person> loadPersons() {
+        try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileStringPath))){
+            return (List<Person>)ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static void savePersons(List<Person> persons) {
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileStringPath));
+            oos.writeObject(persons);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     private static void addTextAtLine(String text, int lineNumber){
